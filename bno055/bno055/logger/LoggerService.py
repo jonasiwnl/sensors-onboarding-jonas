0b72f4daf146ac88389e780ea46c957ad46903a6
arael <jonasg@umich.edu>
2023-10-22 17:52:37 -0400

    rewrite velocity and clean up acceleration

--- bno055/bno055/logger/LoggerService.py
+++ bno055/bno055/logger/LoggerService.py
@@ -16,23 +16,26 @@ class LoggerService:
             self.logging_callback,
         10)
 
-        # Assume IMU is initialized facing up
-        self.z_orientation = True
-
         # Assume IMU is initialized not moving
-        self.last_acceleration = None
-        self.last_time = node.get_clock().now()
+        self.velocity.x = self.velocity.y = self.velocity.z = 0
+        self.acceleration.x = self.acceleration.y = self.acceleration.z = 0
+        self.time = node.get_clock().now().nanoseconds * 1e-9
+        self.z_orientation = False # Assume IMU is initialized facing up
 
     def logging_callback(self, msg):
-        time = self.node.get_clock().now()
-        # self.node.get_logger().info('INFO @ %s' % time)
+        # Task 1: Log linear velocity and orientation
+        new_time = self.node.get_clock().now().nanoseconds * 1e-9
+        delta = self.time - new_time
+        self.velocity.x += msg.linear_acceleration.x * delta
+        self.velocity.y += msg.linear_acceleration.y * delta
+        self.velocity.z += msg.linear_acceleration.z * delta
 
-        # Task 1: Log velocity and orientation
         self.node.get_logger().info('Orientation (%.2f, %.2f, %.2f, %.2f)'
-                               % (msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w),
+                               % (msg.orientation.x, msg.orientation.y,
+                                  msg.orientation.z, msg.orientation.w),
                                throttle_duration_sec=1.0)
         self.node.get_logger().info('Velocity: (%.2f, %.2f, %.2f)'
-                                % (msg.angular_velocity.x, msg.angular_velocity.y, msg.angular_velocity.z),
+                                % (self.velocity.x, self.velocity.y, self.velocity.z),
                                throttle_duration_sec=1.0)
 
         # Task 2: Log when IMU flips over
@@ -42,17 +45,15 @@ class LoggerService:
             self.node.get_logger().warn('IMU flipped over.')
             self.z_orientation = not self.z_orientation
 
-        """
         # Task 3: Log sudden acceleration changes
-        if self.last_acceleration:
-            distance = sqrt(
-                pow(msg.linear_acceleration.x - self.last_acceleration.x, 2) +
-                pow(msg.linear_acceleration.y - self.last_acceleration.y, 2) +
-                pow(msg.linear_acceleration.z - self.last_acceleration.z, 2) )
+        accel_delta = sqrt(
+            pow(msg.linear_acceleration.x - self.acceleration.x, 2) +
+            pow(msg.linear_acceleration.y - self.acceleration.y, 2) +
+            pow(msg.linear_acceleration.z - self.acceleration.z, 2) )
 
-            # TODO ?
-            if distance / (time - self.last_time) > self.ACCELERATION_THRESHOLD:
-                self.node.get_logger().warn('Sudden acceleration change.')
-        self.last_acceleration = msg.linear_acceleration
-        self.last_time = time
-        """
+        # TODO ?
+        if accel_delta / delta > self.ACCELERATION_THRESHOLD:
+            self.node.get_logger().warn('Sudden acceleration change.')
+
+        self.acceleration = msg.linear_acceleration
+        self.time = new_time
